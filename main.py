2cea7a2ea7d8511f917d552d9b61d110ba96bb94
JanCG <jancg@outlook.de>
2024-04-09 22:57:23 +0200

    Adding stuff

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def get_user_data(user):
     name, tag = user.strip().split("#")
     account_api = 'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + name + '/' + tag + '?api_key=' + api_key
     puuid = re.get(account_api).json()["puuid"]
+    if puuid == None:
+        pass
     st.text("Eingegebener name: " + name + "#" + tag)
     games_api = 'https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/' + puuid + '/ids?' + 'queue=420' + '&start=0&count=20' + '&api_key=' + api_key
     games = re.get(games_api).json()
