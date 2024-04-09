5dafe5a7c306f4736015f63e9e3ee90b80621562
JanCG <jancg@outlook.de>
2024-04-09 22:53:04 +0200

    Adding stuff

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ api_key = "RGAPI-a54832df-ff96-4069-87ca-e3a2089c0333"
 def get_user_data(user):
     name, tag = user.strip().split("#")
     account_api = 'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + name + '/' + tag + '?api_key=' + api_key
-    account_details = re.get(account_api).json()
+    account_details = re.get(account_api).json()["puuid"]
     st.text("Eingegebener name: " + name + "#" + tag)
     st.text(account_details)
     pass
