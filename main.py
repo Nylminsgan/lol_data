3b819e2fc46d5f46c7d97613d8f22400331651d6
JanCG <jancg@outlook.de>
2024-04-09 22:26:41 +0200

    Adding stuff

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests as re
 
 API = "RGAPI-a54832df-ff96-4069-87ca-e3a2089c0333"
-account_api = 'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + name + '/' + tag + '?api_key=' + api_key
+#account_api = 'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + name + '/' + tag + '?api_key=' + api_key
 
 def get_user_data(user):
     name, tag = user.strip().split("#")
