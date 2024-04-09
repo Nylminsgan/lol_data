f133d1db6a5ccc980a22aca647c5c691277b83c6
JanCG <jancg@outlook.de>
2024-04-09 20:10:17 +0200

    Adding stuff

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-st.text("Hello World!")
-with st.form(key='my_form'):
-   username = st.text_input("Username#Tag")
-   st.form_submit_button('analyze')
 
-# Just add it after st.sidebar:
-a = st.sidebar.radio('Choose:',[1,2])
+def get_user_data(user):
+    print("get_user_data")
+    pass
+
+
+st.text("Hello World!")
+username = st.text_input("Username#Tag")
+if st.button("Analyze"):
+    get_user_data(username)
+
+
+
