8629ed8036d47eaf415b2aeae84e5d38565ab430
JanCG <jancg@outlook.de>
2024-04-09 23:39:03 +0200

    Adding stuff

--- main.py
+++ main.py
@@ -4,30 +4,36 @@ import requests as re
 
 api_key = "RGAPI-a54832df-ff96-4069-87ca-e3a2089c0333"
 
-def get_match_data(match_id):
-    match_data_req = "https://europe.api.riotgames.com/lol/match/v5/matches/" + match_id + '?api_key=' + api_key
-    return re.get(match_data_req).json()
-def get_user_match_data(user):
+def get_user(user):
     name, tag = user.strip().split("#")
     account_api = 'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + name + '/' + tag + '?api_key=' + api_key
     acc = re.get(account_api)
     if acc.ok:
-        acc = acc.json()
-        st.text("Eingegebener name: " + acc["gameName"] + "#" + acc["tagLine"])
-        games_api = 'https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/' + acc["puuid"] + '/ids?' + 'queue=420' + '&start=0&count=20' + '&api_key=' + api_key
-        games = re.get(games_api).json()
-        return games
+        return acc.json()
     else:
-        st.text("Name nicht gefunden")
-        return ""
+        return "Name nicht gefunden"
+
+def get_matches(acc, amount=20):
+    games_api = 'https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/' + acc["puuid"] + '/ids?' + 'queue=420' + '&start=0&count='+ amount + '&api_key=' + api_key
+    games = re.get(games.api)
+    if games.ok:
+        return games.json()
+    else:
+        return "Name nicht gefunden"
+
+def get_match_data(match_id):
+    match_data_req = "https://europe.api.riotgames.com/lol/match/v5/matches/" + match_id + '?api_key=' + api_key
+    return re.get(match_data_req).json()
 
 
 st.text("Hello World!")
 username = st.text_input("Username#Tag")
 if st.button("Analyze"):
-    games = get_user_match_data(username)
-    match = get_match_data(games[0])
-    st.text(match)
+    user = get_user(username)
+    st.json(user)
+    games = get_matches(user)
+    st.json(games)
+
 
 
 
