caf0f91a4816921fed95f417a0bea2c353c0d16a
JanCG <jancg@outlook.de>
2024-04-09 19:38:35 +0200

    Adding stuff

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.text("Hello World!")
+username = st.text_area("Username + #")
 
 # Just add it after st.sidebar:
 a = st.sidebar.radio('Choose:',[1,2])
-print(a)
